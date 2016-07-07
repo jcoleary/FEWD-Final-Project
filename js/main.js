00cc6f87dc36f84ffe9a609fde254b9da28ed3c6
James O'Leary <jamesoleary@Jamess-MacBook-Air.local>
2016-07-07 18:21:40 -0500

    Laid framework for continental information div

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
 function showAfrica(){
 	document.getElementById("worldMap").style.display="none";
-	document.getElementById("africaSat").style.display="block" height="801.31579px" width="900px";
-}""
+	document.getElementById("africaSat").style.display="block" 
