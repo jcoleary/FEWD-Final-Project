1089cc07f6b69ac689532b92efe6f30f235291b2
James O'Leary <jamesoleary@Jamess-MacBook-Air.local>
2016-07-06 20:35:09 -0500

    Further testing Africa image, take 3

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 function showAfrica(){
 	document.getElementById("worldMap").style.display="none";
-	document.getElementById("africaSat").style.display="block";
-}
+	document.getElementById("africaSat").style.display="block" height="801.31579px" width="900px";
+}""
