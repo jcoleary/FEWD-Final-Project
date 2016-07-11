3bdc7234d09a14e6cc924b926dfb8b271db1562e
James O'Leary <jamesoleary@Jamess-MacBook-Air.local>
2016-07-11 18:14:59 -0500

    Deploying ContinentProfiles for use

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
-    $( '.dropdown' ).hover(
-        function(){
+    $( '.dropdown' ).hover(function(){
             $(this).children('.sub-menu').slideDown(200);
+            });
+
+
+	$('#africaButton').on('click', function(){
+		
+	});
+
 })
+
