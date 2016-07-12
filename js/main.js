a4cf72d5cd08da1b6dfb0f84eef5dfa8f415fdfa
James O'Leary <jamesoleary@Jamess-MacBook-Air.local>
2016-07-11 19:34:21 -0500

    Repositioned buttons, wrote content for Africa button, gave button functionality

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
-    $( '.dropdown' ).hover(function(){
-            $(this).children('.sub-menu').slideDown(200);
-            });
+    // $( '.dropdown' ).hover(function(){
+    //         $(this).children('.sub-menu').slideDown(200);
+    //         });
 
 
 	$('#africaButton').on('click', function(){
-		
+		$(".sample").show();
+		$.ajax({
+			type: 'get',
+			url:"http://jcoleary.github.io/continent_profiles"
+		}).done(function(content){
+			var newp = $(content).filter('p');
+			var newText = newp.text();
+			$('.sample').text(newText);
+		});
 	});
 
 })
