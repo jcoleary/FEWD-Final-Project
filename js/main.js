2429f9a94b989deb7c102a2ee08910ceff03bdff
James O'Leary <jamesoleary@Jamess-MacBook-Air.local>
2016-07-11 20:08:46 -0500

    Added button functionality and basic content

--- js/main.js
+++ js/main.js
@@ -16,5 +16,66 @@ $(document).ready(function() {
 		});
 	});
 
-})
+$('#asiaButton').on('click', function(){
+		$(".sample").show();
+		$.ajax({
+			type: 'get',
+			url:"http://jcoleary.github.io/continent_profiles"
+		}).done(function(content){
+			var newp = $(content).filter('p');
+			var newText = newp.text();
+			$('.sample').text(newText);
+		});
+	});
 
+$('#australiaButton').on('click', function(){
+		$(".sample").show();
+		$.ajax({
+			type: 'get',
+			url:"http://jcoleary.github.io/continent_profiles"
+		}).done(function(content){
+			var newp = $(content).filter('p');
+			var newText = newp.text();
+			$('.sample').text(newText);
+		});
+	});
+
+$('#europeButton').on('click', function(){
+		$(".sample").show();
+		$.ajax({
+			type: 'get',
+			url:"http://jcoleary.github.io/continent_profiles"
+		}).done(function(content){
+			var newp = $(content).filter('p');
+			var newText = newp.text();
+			$('.sample').text(newText);
+		});
+	});
+
+$('#naButton').on('click', function(){
+		$(".sample").show();
+		$.ajax({
+			type: 'get',
+			url:"http://jcoleary.github.io/continent_profiles"
+		}).done(function(content){
+			var newp = $(content).filter('p');
+			var newText = newp.text();
+			$('.sample').text(newText);
+		});
+	});
+
+$('#saButton').on('click', function(){
+		$(".sample").show();
+		$.ajax({
+			type: 'get',
+			url:"http://jcoleary.github.io/continent_profiles"
+		}).done(function(content){
+			var newp = $(content).filter('p');
+			var newText = newp.text();
+			$('.sample').text(newText);
+		});
+	});
+
+
+
+})
